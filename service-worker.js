655ef2b9d2210d0464fbb3ac5e98cde460107f1e
waxime <aveline.maxime@gmail.com>
2019-04-10 18:20:53 +0200

    service-worker: Put major items in cache

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
 console.log('In service worker');
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+      caches.open('v1').then(cache => {
+        cache.addAll([
+          '/',
+          'index.html',
+          '/js/app.js',
+          '/js/components/card/card.js',
+          '/js/components/card/card.css',
+        ]);
+      }),
+  );
+});
